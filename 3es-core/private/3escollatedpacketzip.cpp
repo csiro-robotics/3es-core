c976a4adb273f194cbd4f54108c11aa77951410e
Kazys Stepanas <kazys.stepanas@csiro.au>
2018-11-29 09:44:53 +1000

    Minor fix when building without zlib.

--- 3es-core/private/3escollatedpacketzip.cpp
+++ 3es-core/private/3escollatedpacketzip.cpp
@@ -8,7 +8,11 @@ namespace tes
     3, // CL_Low,
     5, // CL_Medium
     7, // CL_High,
+#ifdef TES_ZLIB
     Z_BEST_COMPRESSION // CL_VeryHigh (9)
+#else // TES_ZLIB
+    9
+#endif // TES_ZLIB
   };
 
 #ifdef TES_ZLIB
